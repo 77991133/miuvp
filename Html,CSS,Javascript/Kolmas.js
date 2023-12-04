68c8bea8420a3772e09eda796a1f1ced32813269
Agnes Kivistik <kivistik.agnes@gmail.com>
2023-12-04 16:58:34 +0200

    lisasin pildikausta

--- Html,CSS,Javascript/Kolmas.js
+++ Html,CSS,Javascript/Kolmas.js
@@ -2,7 +2,7 @@ const cafes = {
     "Vaffel kohvik": {
         name: "Vaffel kohvik",
         description: "Description for Vaffel kohvik.",
-        image: "Vahvel-1.jpeg",
+        image: "pildid/Vahvel-1.jpeg",
         menu: "Menüü leiate kohviku kodulehelt",
         openingHours: " esmaspäev suletud <br> teisipäev-reede 11-20 <br> laupäev 9-20 <br> pühapäev 9-19",
         phone: "5 888 3838",
@@ -12,7 +12,7 @@ const cafes = {
     "Dirhami kalakohvik": {
         name: "Dirhami kalakohvik",
         description: "Description for Dirhami kalakohvik.",
-        image: "Dirham_2.jpg",
+        image: "pildid/Dirham_2.jpg",
         menu: "Menüü muutub neil iga nädal, mõinkord isegi iga päev.",
         openingHours: "Ettebronneerimine",
         phone: "5 177 899",
@@ -22,7 +22,7 @@ const cafes = {
     "LAUT resto": {
         name: "LAUT resto",
         description: "Description for LAUT resto.",
-        image: "LAUT_2.jpg",
+        image: "pildid/LAUT_2.jpg",
         menu: "Näidis menüü kodulehel.",
         openingHours: "Ettebronneerimine",
         phone: "5222693",
@@ -32,7 +32,7 @@ const cafes = {
     "Emmaste teemaja": {
         name: "Emmaste teemaja",
         description: "Description for LAUT resto.",
-        image: "emmaste-teemaja_3.jpg",
+        image: "pildid/emmaste-teemaja_3.jpg",
         menu: "Menüüst kilde võin näha teemaja instagrammis.",
         openingHours: "Avatud igapäevaselt <br> 11:00-17:00",
         phone: "5 342 3957",
@@ -42,7 +42,7 @@ const cafes = {
     "Rooslaiu farmi kohvik": {
         name: "Rooslaiu farmi kohvik",
         description: "Description for LAUT resto.",
-        image: "Roosilaiu_2.webp",
+        image: "pildid/Roosilaiu_2.webp",
         menu: "Menüü leiate kodulehelt.",
         openingHours:"01.06.23-31.08.23 <br> 09:00 - 17:00 <br> 01.09.23-31.05.24 <br> Ainult ettetellimisel.",
         phone: "5 164 923",
@@ -52,7 +52,7 @@ const cafes = {
     "Eastern Outback kohvik": {
         name: "Eastern Outback kohvik",
         description: "Description for LAUT resto.",
-        image: "Outback_1.jpg",
+        image: "pildid/Outback_1.jpg",
         menu: "Menüüga saate tutvuda kohapeal.",
         openingHours: "esmaspäev-neljapäev 11-16 <br> reede 11-19 <br> laupäev-pühapäev Kinni",
         phone: "5 559 0913",
@@ -61,7 +61,7 @@ const cafes = {
     "Rannaresto": {
         name: "Rannaresto",
         description: "Description for LAUT resto.",
-        image: "rannaresto-1.jpg",
+        image: "pildid/rannaresto-1.jpg",
         menu: "Menüü leiate kodulehelt.",
         openingHours: "esmaspäev-neljapäev, pühapäev 11:00 - 22:00 <br> reede-laupäev: 11:00 - 01:00",
         phone: "5 907 2255",
@@ -71,7 +71,7 @@ const cafes = {
     "Kapteni Farmi kalakohvik": {
         name: "Kapteni Farmi kalakohvik",
         description: "Description for LAUT resto.",
-        image: "kapten_1.jpg",
+        image: "pildid/kapten_1.jpg",
         menu: "Menüüga saate tutvuda kodulehel.",
         openingHours: "26.06.23-31.08.23 <br> esmaspäev-pühapäev 12:00 - 21:00 <br> 01.09.23-30.04.24 <br> Ainult ettetellimisel",
         phone: "5 134 683",
@@ -81,7 +81,7 @@ const cafes = {
     "Kohvik Lohvik": {
         name: "Kohvik Lohvik",
         description: "Description for LAUT resto.",
-        image: "Lohvik_1.jpg",
+        image: "pildid/Lohvik_1.jpg",
         menu: "Menüüga saate tutvuda instagrammis.",
         openingHours: "Avatud 01.05 - 30.09 igapäevaselt: 10:00 - 20:00",
         phone: "5 567 1598",
@@ -90,7 +90,7 @@ const cafes = {
     "Pilguse Mõisa resto": {
         name: "Pilguse Mõisa resto",
         description: "Description for LAUT resto.",
-        image: "Pilguse_2.jpg",
+        image: "pildid/Pilguse_2.jpg",
         menu: "Menüüga saate tutvuda kodulehel.",
         openingHours:"01.05-30.08 <br>esmaspäev-neljapäev, pühapäev 12:00 - 21:30 <br>reede-laupäev: 12:00 - 22:00 <br>01.05-01.11 <br>Avatud ainult ettetellimisel", 
         phone: "454 5445",
@@ -100,7 +100,7 @@ const cafes = {
     "Roheline Maja kohvik ja pood": {
         name: "Roheline Maja kohvik ja pood",
         description: "Description for LAUT resto.",
-        image: "Roheline_maja_3.jpg",
+        image: "pildid/Roheline_maja_3.jpg",
         menu: "Menüüga tutvuda kodulehel.",
         openingHours:"teisipäev-reede: 09:00 - 17:00 <br>laupäev 09:00 - 18:00<br>pühapäev 10:00 - 16:00<br>esmaspäev suletud",
         phone: "434 4307",
@@ -110,7 +110,7 @@ const cafes = {
     "Cruffin Bread Factory": {
         name: "Cruffin Bread Factory",
         description: "Description for LAUT resto.",
-        image: "Cruffin_3.jpeg",
+        image: "pildid/Cruffin_3.jpeg",
         menu: "Menüüga saate tutvuda kodulehel.",
         openingHours: "esmaspäev-pühapäev 10-18",  
         phone: "5 617 1021",
